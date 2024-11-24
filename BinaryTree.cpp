30dca509cb6b27426fee9416a0ab695437660474
Jairo Calcina <jairo.calcina@outlook.com>
2024-11-24 20:52:57 -0300

    feat: add recursive tree traversal

---
+++ BinaryTree.cpp
@@ -0,0 +1,31 @@
+#include "BinaryTree.h"
+
+void BinaryTree::preorder(Node* root, std::queue<int>* queue)
+{
+	if (root == nullptr)
+		return;
+
+	queue->push(root->get_data());
+	this->preorder(root->get_left(), queue);
+	this->preorder(root->get_right(), queue);
+}
+
+void BinaryTree::inorder(Node* root, std::queue<int>* queue)
+{
+	if (root == nullptr)
+		return;
+
+	this->inorder(root->get_left(), queue);
+	queue->push(root->get_data());
+	this->inorder(root->get_right(), queue);
+}
+
+void BinaryTree::postorder(Node* root, std::queue<int>* queue)
+{
+	if (root == nullptr)
+		return;
+
+	this->postorder(root->get_left(), queue);
+	this->postorder(root->get_right(), queue);
+	queue->push(root->get_data());
+}
